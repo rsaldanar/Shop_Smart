33b3ab35656c35f3c431d7194b543f5fd79fa8a0
Rafael Saldaña R <r.saldana@dominicanadeseguros.com>
2021-10-25 22:16:01 -0400

    Inicio del proyecto

---
+++ main/java/com/bytecode/shop_smart/ShopSmartApplication.java
@@ -0,0 +1,13 @@
+package com.bytecode.shop_smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSmartApplication.class, args);
+	}
+
+}
---
+++ test/java/com/bytecode/shop_smart/ShopSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecode.shop_smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
